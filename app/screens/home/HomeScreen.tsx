4de70e7dd83f9cc5a378fa848b12b509196b7b6b
Sebastiao Lutonda <lutonda@gmail.com>
2023-11-01 21:47:51 +0100

    fixer

---
+++ app/screens/home/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import { Button } from "react-native";
+
+const HomeScreen = ({ navigation }: any) => {
+    return (
+        <Button
+            title="Go to Jane's profile"
+            onPress={() =>
+                navigation.navigate('Profile', { name: 'Jane' })
+            }
+        />
+    );
+};
